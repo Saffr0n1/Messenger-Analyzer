63a98b98c378371c0bff98720aabbb1e337dbbbc
Abishrant Panday <abishrantpanday@college.harvard.edu>
2020-07-13 22:24:11 -0400

    Finalize TextParser

---
+++ MessageAggregate.py
@@ -0,0 +1,5 @@
+# Abishrant Panday 2020
+# Allows users to combine multiple message.html files into one for easier analysis
+
+import TextParser
+
--- TextParser.py
+++ TextParser.py
@@ -6,24 +6,32 @@
 
 import re
 
-
+# Converts single-line message.html file into string
 def text_to_string(file):
     rawMessage = open(file, 'r', encoding="utf8")
     rawString = rawMessage.read()
     return rawString
 
-
+# From message string, parses triplets containing sender, message, time sent
 def text_filter(string):
     coarseFilter = "<div class=\"pam _3-95 _2pi0 _2lej uiBoxWhite noborder\"><div class=\"_3-96 _2pio _2lek _2lel\">" \
                    "[a-zA-Z0-9_,;'\"\s.?!]+<\/div><div class=\"_3-96 _2let\"><div><div><\/div><div>[a-zA-Z0-9_,:;'’\"\s.?!(&#039;)]" \
                    "+<\/div><div><\/div><div><\/div><\/div><\/div><div class=\"_3-94 _2lem\">[a-zA-Z0-9_,;:'\"\s.?!]+<\/div><\/div>";
     rawText = re.findall(coarseFilter, string)
-    senderFilter =
-    messageFilter =
-    timeFilter =
-    return len(rawText)
 
+    localFilter = ">[a-zA-Z0-9_,;:'\"\s.?!]+<"
+    rawFinal = []
+    for item in rawText:
+        rawFinal.append(re.findall(localFilter, item))
 
-print(text_filter(text_to_string("Data\message_1.html")))
-#     for i in range(len(rawText)):
-#         rawText[i] = rawText[i][16:-28]
+    return rawFinal
+
+def cleanse(list):
+    for item in list:
+        for i in range(len(item)):
+            item[i] = item[i][1:-1]
+
+    return list[0]
+
+print(cleanse(text_filter(text_to_string("Data\message_1.html"))))
+
