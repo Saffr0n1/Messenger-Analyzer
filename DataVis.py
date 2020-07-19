a6bae071612546c1173adbb124deace1dbb949f8
Abishrant Panday <abishrantpanday@college.harvard.edu>
2020-07-19 19:58:59 -0400

    Data Vis Changes

--- DataVis.py
+++ DataVis.py
@@ -1,13 +1,74 @@
 # Abishrant Panday 2020
-# General helper file that contains many useful seaborn data visualization functions
+# General helper file that contains many useful data visualization functions
 # Allows viewing relationships in Messenger data visually
 
 import MessageAggregate
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 i = MessageAggregate.aggregator("Data/")
-print(len(i))
+print(i.groupby(i.index.day)['Name'].value_counts())
+print(i['Name'].value_counts())
 
 
-def time_frequency(inputList, scale):
+def time_frequency(inputList, scale, startRange, endRange, color):
+    if startRange == 'Beginning':
+        startRange = inputList.index[1]
+    if endRange == 'End':
+        endRange = inputList.index[-1]
+
+    inputList = inputList.loc[startRange:endRange]
+
+    if (scale == "Default"):
+        sns.set(rc={'figure.figsize': (18, 4)})
+        inputList["Date"] = inputList.index.round('1d').date
+        il2 = inputList.groupby('Date').agg({'Name': 'value_counts'})
+        reindex(il2,color,"Date")
+
+    if (scale == "Year"):
+        sns.set(rc={'figure.figsize': (4, 4)})
+        il2 = inputList.groupby('Year').agg({'Name': 'value_counts'})
+        reindex(il2,color,"Year")
+
     if (scale == "Month"):
+        sns.set(rc={'figure.figsize': (4, 4)})
+        il2 = inputList.groupby('Month').agg({'Name': 'value_counts'})
+        reindex(il2,color,"Month")
+
+    if (scale == "Day"):
+        sns.set(rc={'figure.figsize': (4, 4)})
+        il2 = inputList.groupby('Weekday').agg({'Name': 'value_counts'})
+        reindex(il2,color,"Weekday")
+
+
+def reindex(inputList, color,index):
+    inputList.columns = ['Frequencies']
+    pivot = pd.pivot_table(inputList.reset_index(), values='Frequencies', index=index, columns='Name').fillna(0)
+    ax = pivot.plot(kind='bar', stacked=True, color=color)
+    ax.set_xticklabels(labels=pivot.index, rotation=70, rotation_mode="anchor", ha="right")
+    ax.set_ylabel("Number of Messages")
+    ax.set_xlabel(index)
+    plt.legend()
+    plt.show()
+
+# def all_frequency(inputList, startRange, endRange):
+#     if startRange == 'Beginning':
+#         startRange = inputList.index[1]
+#     if endRange == 'End':
+#         endRange = inputList.index[-1]
+#
+#     inputList = inputList.loc[startRange:endRange]
+
+
+
+
+
+
+
+
+
+
+
+
 
--- MessageAggregate.py
+++ MessageAggregate.py
@@ -1,9 +1,10 @@
 # Abishrant Panday 2020
 # Allows users to automatically upload all html files in a given folder for easier analysis
+import numpy
 
 import TextParser
 import glob
-inport numpy as np
+import pandas as pd
 
 # path_to_dir is the path to the folder containing html messages
 def aggregator(path_to_dir):
@@ -16,6 +17,14 @@ def aggregator(path_to_dir):
         for item in c:
             bigList.append(item)
 
-    return bigList
+    listDF = pd.DataFrame(bigList, columns=['Name', 'Message', 'Date Time'])
+    listDF['Date Time'] = pd.to_datetime(listDF['Date Time'])
+    listDF = listDF.set_index('Date Time')
+    listDF['Year'] = listDF.index.year
+    listDF['Month'] = listDF.index.month
+    listDF['Day'] = listDF.index.day
+    listDF['Time'] = listDF.index.time
+    listDF['Weekday'] = listDF.index.day_name()
+    return listDF
 
-print(aggregator("Data/")[0])
+
